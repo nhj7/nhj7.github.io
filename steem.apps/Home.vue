95999685641fb5d43c008dd182243d3df32dcab5
nhj7 <nhj7@gmail.com>
2017-11-11 04:13:42 +0900

    update source.

---
+++ steem.apps/Home.vue
@@ -0,0 +1,113 @@
+<template>
+<div>
+
+<div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-block">
+        <h3 class="card-title">Steem Me</h3>
+        <p class="card-text">Useful site to show hidden information. created by @asbear</p>
+        <a href="https://ianpark.github.io/steemme/" target="_blank" class="btn btn-primary">Go Steem Me</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-block">
+        <h3 class="card-title">Steem Tool</h3>
+        <p class="card-text">The Steemit tool suite. created by @segyepark</p>
+        <a href="http://tool.steem.world" target="_blank" class="btn btn-primary">Go Steem Tool</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-block">
+        <h3 class="card-title">Steem Us</h3>
+        <p class="card-text">steemit stats and portals. created by @jungs</p>
+        <a href="http://www.steemus.com" target="_blank" class="btn btn-primary">Go Steem Us</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-block">
+        <h3 class="card-title">MukSteem</h3>
+        <p class="card-text">Korean Stimian's Restaurant Collection. created by @lee2</p>
+        <a href="http://muksteem.com" target="_blank" class="btn btn-primary">Go MukSteem</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-block">
+        <h3 class="card-title">Under Steemed</h3>
+        <p class="card-text">Undervalued posting excavation site. created by @tabris</p>
+        <a href="https://sydneyitguy.github.io/understeemed" target="_blank" class="btn btn-primary">Go Under Steemed</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-block">
+        <h3 class="card-title">Steem Friend</h3>
+        <p class="card-text">Who would be best friend of me? created by @jeongmincha</p>
+        <a href="http://steemfriend.com/" target="_blank" class="btn btn-primary">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-block">
+        <h3 class="card-title">Steemit Post Statistics</h3>
+        <p class="card-text">Steemit Post Statistics site. created by @segyepark</p>
+        <a href="https://iworldpark.github.io/steemit/stat/post/" target="_blank" class="btn btn-primary">Go Statistics</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-block">
+        <h3 class="card-title">Steemlit</h3>
+        <p class="card-text">Distributing SBD to multiple receivers. created by @asbear</p>
+        <a href="https://ianpark.github.io/steemlit/" target="_blank" class="btn btn-primary">Go Steemlit</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+<div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-block">
+        <h3 class="card-title">Steem Now</h3>
+        <p class="card-text">What if I wonder about my voting power? created by @penguinpablo</p>
+        <a href="https://steemnow.com/" target="_blank" class="btn btn-primary">Go Steem Now</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-block">
+        <h3 class="card-title">Cobot</h3>
+        <p class="card-text">All Coin Ticker. created by @nhj12311</p>
+        <a href="http://cobot.co.kr" target="_blank" class="btn btn-primary">Go Cobot</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+</div>
+</template>
