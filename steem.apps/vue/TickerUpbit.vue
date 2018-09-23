8f7bea7bad7dd9b10a8e7cab1323a913025bcf72
HyungJu Na <nhj7@HyungJuui-MacBook-Pro.local>
2018-09-24 00:54:58 +0900

    update add vue dir, add upbit ticker vue.

---
+++ steem.apps/vue/TickerUpbit.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <iframe id="iframeUpbit" width="100%" scrolling="no" v-on:load="resizeIframe()" src="./test/test_upbit.html"></iframe>
+  </div>
+</template>
+<script>
+function resizeIframe() {
+  var obj = document.getElementById("iframeUpbit");
+  console.log(obj, obj.contentWindow.document.body.scrollHeight)
+  //obj.style.height = 0;
+  obj.style.height = obj.contentWindow.document.body.scrollHeight+'px';
+
+}
+
+
+module.exports = {
+  name: 'TickerUpbit',
+  data: function () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+    resizeIframe : function(obj){ resizeIframe(obj); }
+  }
+}
+</script>
+</script>
