61a4d0f7880c611397d0484708bab85a3356b110
HyungJu Na <nhj7@HyungJuui-MacBook-Pro.local>
2018-09-24 01:48:43 +0900

    update. height.

--- steem.apps/vue/TickerUpbit.vue
+++ steem.apps/vue/TickerUpbit.vue
@@ -1,21 +1,30 @@
 <template>
   <div>
-    <iframe id="iframeUpbit" width="100%" frameborder="0" scrolling="no" v-on:load="resizeIframe()" src="./test/test_upbit.html?v=1"></iframe>
+    <div id="tmp">
+
+    </div>
+    <div class="frameWrapper">
+      <iframe id="iframeUpbit" name="iframeUpbit" width="100%" frameborder="0" scrolling="no" v-on:load="resizeIframe()" src="./test/test_upbit.html?v=1"></iframe>
+    </div>
   </div>
+
 </template>
 <script>
-function resizeIframe() {
+function resizeIframe1() {
 
-  $('#iframeUpbit').iFrameResize({log:true, checkOrigin:false, inPageLinks:true, sizeWidth:true});
-  /*
-  var obj = document.getElementById("iframeUpbit");
-  console.log(obj, obj.contentWindow.document.body.scrollHeight)
-  //obj.style.height = 0;
-  obj.style.height = obj.contentWindow.document.body.scrollHeight+'px';
-  */
-  
 }
 
+function resizeIframe(){
+ //find the height of the internal page
+
+ var the_height=document.getElementById('iframeUpbit').contentWindow.document.body.scrollHeight;
+ $("#tmp").text(the_height);
+ //change the height of the iframe
+ document.getElementById('iframeUpbit').style.height=the_height + "px";
+
+ //document.getElementById('the_iframe').scrolling = "no";
+ document.getElementById('iframeUpbit').style.overflow = "hidden";
+}
 
 module.exports = {
   name: 'TickerUpbit',
@@ -28,8 +37,29 @@ module.exports = {
 
   },
   methods: {
-    resizeIframe : function(obj){ resizeIframe(obj); }
+    resizeIframe : function(){ resizeIframe(); }
+  }
+  , mounted : function() {
+
   }
 }
 </script>
-</script>
+<style>
+.frameWrapper {
+  position: relative;
+  padding-bottom: 65.25%;
+  padding-top: 30px;
+  height: 0;
+
+  -webkit-overflow-scrolling:touch;
+
+}
+.frameWrapper iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
+}
+</style>
